510c16d3cad4f9637675d9042d8345826e2833a1
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-06-05 18:28:00 +0530

    Used constants for messages

--- Signup FORM/script.js
+++ Signup FORM/script.js
@@ -1,3 +1,15 @@
+// Constants for storing different messages
+const u_name_empty = "Please enter username";
+const pass_empty = "Please enter password";
+const cf_pass_empty = "Please enter password again";
+const email_empty = "Please enter email address";
+const checkbox_empty = "Please select at least one skill";
+const email_invalid = "Please enter a valid e-mail address.";
+const pass_length = "Please enter at least 8 characters in password";
+const pass_not_match = "Password did not match: Please try again...";
+const captcha_empty = "Please Enter CAPTCHA Code";
+const captcha_not_match = "The CAPTCHA Code Does Not Match";
+
 function validate(thisform) {
     var name = thisform.username.value;
     var pass = thisform.password.value;
@@ -6,63 +18,63 @@ function validate(thisform) {
 
     // Validation for required fields should not be left empty
     if (name == "" || name == null) {
-        window.alert("Please enter username");
+        window.alert(u_name_empty);
         document.thisform.username.focus();
         return false;
     }
 
     if (pass == "" || pass == null) {
-        window.alert("Please enter password");
+        window.alert(pass_empty);
         document.thisform.password.focus();
         return false;
     }
 
     if (cfpass == "" || cfpass == null) {
-        window.alert("Please enter password again");
+        window.alert(cf_pass_empty);
         document.thisform.cfpassword.focus();
         return false;
     }
 
     if (email == "" || email == null) {
-        window.alert("Please enter email address");
+        window.alert(email_empty);
         document.thisform.email.focus();
         return false;
     }
 
     if (!thisform.s1.checked && !thisform.s2.checked && !thisform.s3.checked && !thisform.s4.checked && !thisform.s5.checked) {
-        window.alert("Please select at least one skill");
+        window.alert(checkbox_empty);
         return false;
     }
 
     //Validation for valid email
     if (email.indexOf("@", 0) < 0) {
-        window.alert("Please enter a valid e-mail address.");
+        window.alert(email_invalid);
         document.thisform.email.focus();
         return false;
     }
 
     if (email.indexOf(".", 0) < 0) {
-        window.alert("Please enter a valid e-mail address.");
+        window.alert(email_invalid);
         document.thisform.email.focus();
         return false;
     }
 
     //Validation for length of password to be greater than 8
     if (pass.length < 8) {
-        window.alert("Please enter at least 8 characters in password");
+        window.alert(pass_length);
         document.thisform.password.focus();
         return false;
     }
 
     if (cfpass.length < 8) {
-        window.alert("Please enter at least 8 characters in password");
+        window.alert(pass_length);
         document.thisform.cfpassword.focus();
         return false;
     }
 
     //Validation to check if both passwords are same or not
     if (pass != cfpass) {
-        window.alert("Password did not match: Please try again...")
+        window.alert(pass_not_match);
         document.thisform.cfpassword.focus();
         return false;
     }
@@ -71,11 +83,11 @@ function validate(thisform) {
     var why = "";
 
     if (thisform.CaptchaInput.value == "") {
-        why += "- Please Enter CAPTCHA Code.\n";
+        why += captcha_empty;
     }
     if (thisform.CaptchaInput.value != "") {
         if (ValidCaptcha(thisform.CaptchaInput.value) == false) {
-            why += "- The CAPTCHA Code Does Not Match.\n";
+            why += captcha_not_match;
         }
     }
     if (why != "") {
