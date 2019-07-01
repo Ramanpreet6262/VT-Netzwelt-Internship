b62fb3f821db0c42f7932314d5a6654be2d67b4e
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-07-01 19:51:50 +0530

    connected mysql database and implemented get request using it

--- Node-Rest-Api/server.js
+++ Node-Rest-Api/server.js
@@ -1,16 +1,26 @@
-var express = require('express');        // call express
-var app = express();                 // define our app using express
-var bodyParser = require('body-parser');
+var express = require('express'); // call express
+var app = express(); // define our app using express
+var mysql = require('mysql'); // Calling mysql module
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var port = process.env.PORT || 8080; // set our port
 
-var port = process.env.PORT || 8080;        // set our port
+// Creating mysql connection 
+app.use(function (req, res, next) {
+    res.locals.connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'netzwelt',
+        database: 'learningnode'
+    });
+    res.locals.connection.connect(function (err) {
+        if (err) throw err;
+        console.log("connected to database");
+    });
+    next();
+});
 
 // ROUTES FOR OUR API
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 
 // middleware to use for all requests
 router.use(function (req, res, next) {
@@ -19,23 +29,44 @@ router.use(function (req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
-
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({
+        message: 'hooray! welcome to our api!'
+    });
 });
 
 // more routes
-router.route('/parts')
+router.route('/customers')
 
-    // post request (accessed at POST http://localhost:8080/api/parts)
-    .post(function (req, res) {
-        res.json({ message: 'POST successful!!' });
-    })
-
+    // GET request to retreive all customers 
     .get(function (req, res) {
-        res.json({ message: "GET Successful!!"});
+        res.locals.connection.query("SELECT * from customers", function (error, results, fields) {
+            if (error) {
+                //If there is error, we send the error in the error section with 500 status
+                res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+            } else {
+                //If there is no error, all is good and response is 200OK.
+                res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+            }
+        });
     });
+
+    
+
+
+
+// // post request (accessed at POST http://localhost:8080/api/parts)
+// .post(function (req, res) {
+//     res.json({
+//         message: 'POST successful!!'
+//     });
+// })
+
+
+//con.end();
+
+
 
 
 // REGISTER OUR ROUTES
