ce11498c45ddcb6c7d8b395d096d3d6f4f85b46b
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-07-12 01:20:50 +0530

    Took routes to new file in nodejs ass1

--- NodeJS-Assignments/Assignment 1/ass1.js
+++ NodeJS-Assignments/Assignment 1/ass1.js
@@ -1,41 +1,9 @@
+//importing core module
 const http = require("http");
 
-const server = http.createServer( (req, res) => {
-    const url = req.url;
+//importing custom module
+const routes = require("./routes");
 
-    if(url === "/") {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>1 Ass</title></head>');
-        res.write('<body><h1>Hello All!</h1>');
-        res.write('<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Submit</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if(url === "/users") {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>1 Ass</title></head>');
-        res.write('<body><ul><li>USER1</li><li>USER2</li><li>USER3</li><li>USER4</li></ul></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if(url === "/create-user") {
-        const inputData = [];
-        req.on('data', chunk => {
-            inputData.push(chunk);
-        });
-        req.on('end', () => {
-            const body = Buffer.concat(inputData).toString();
-            console.log(body.split('=')[1]); //username=value_entered_by_user
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
