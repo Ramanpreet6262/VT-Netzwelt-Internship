2394ad9acb7929ef28719c5a99107368882bf71c
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-07-21 21:02:31 +0530

    enhancement

--- Node-Practise/Express_Router/app.js
+++ Node-Practise/Express_Router/app.js
@@ -5,17 +5,17 @@ const rootDir = require('./util/path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, '/views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
