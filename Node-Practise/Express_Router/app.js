aa493180e714bc7a39da2ff9976f7f367fc552fa
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-07-21 19:17:43 +0530

    Added html,css content and path helper function

--- Node-Practise/Express_Router/app.js
+++ Node-Practise/Express_Router/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const rootDir = require('./util/path');
 
 const app = express();
 
@@ -7,12 +9,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found!!</h1>');
+    res.status(404).sendFile(path.join(rootDir, '/views', '404.html'));
 });
 
 app.listen(3000);
