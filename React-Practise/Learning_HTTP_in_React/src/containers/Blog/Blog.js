a4e44366c0b01900990f28955a7460a2c2c25459
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-12-26 01:56:08 +0530

    added post, delete func

--- React-Practise/Learning_HTTP_in_React/src/containers/Blog/Blog.js
+++ React-Practise/Learning_HTTP_in_React/src/containers/Blog/Blog.js
@@ -9,21 +9,28 @@ import './Blog.css';
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null
+    selectedPostId: null,
+    error: false
   };
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
-      const posts = res.data.slice(0, 4);
-      const updatedPosts = posts.map(post => {
-        return {
-          ...post,
-          author: 'Raman'
-        };
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        const posts = res.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Raman'
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        //   console.log(res);
+      })
+      .catch(err => {
+        this.setState({ error: true });
+        // console.log(err);
       });
-      this.setState({ posts: updatedPosts });
-      //   console.log(res);
-    });
   }
 
   postSelectedHandler = id => {
@@ -31,16 +38,23 @@ class Blog extends Component {
   };
 
   render() {
-    const posts = this.state.posts.map(post => {
-      return (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
-      );
-    });
+    let posts = (
+      <p style={{ textAlign: 'center', color: 'red', fontWeight: 'bold' }}>
+        Something went wrong !!!
+      </p>
+    );
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        );
+      });
+    }
 
     return (
       <div>
