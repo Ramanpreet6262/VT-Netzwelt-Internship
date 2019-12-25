42736e1ead5ed06e0caaf3797114e18e532d4a19
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-12-26 01:28:17 +0530

    added GET functionality

---
+++ React-Practise/Learning_HTTP_in_React/src/components/FullPost/FullPost.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './FullPost.css';
+
+class FullPost extends Component {
+  state = {
+    loadedPost: null
+  };
+
+  componentDidUpdate() {
+    if (this.props.id) {
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+          .then(res => {
+            this.setState({ loadedPost: res.data });
+            //   console.log(res);
+          });
+      }
+    }
+  }
+
+  render() {
+    let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
+    }
+    if (this.state.loadedPost) {
+      post = (
+        <div className='FullPost'>
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className='Edit'>
+            <button className='Delete'>Delete</button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
+}
+
+export default FullPost;
+
+/* 
+By using this.setState in componentDidUpdate(),
+We are ending up in an infinite loop of ajax requests
+
+We are calling an ajax call in componentDidUpdate, and we set the state on the callback, 
+that will trigger another call and update which will call the ajax request again and 
+callback will set state again and so on.
+*/
