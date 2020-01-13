f7822b197e2bd5d8f652231c0c042dfcf5ff739c
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-01-13 11:14:56 +0530

    store, reducer and action basics

---
+++ React-Practise/Redux-Practise/redux-basics.js
@@ -0,0 +1,40 @@
+/* This will not include any react code
+It is basic implementation of redux using node */
+
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0
+};
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+  //Never ever mutate any data always do it immutablly................
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
+  return state;
+};
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 6 });
+console.log(store.getState());
+
+// Subscription
+
+// 14 section : 5 + 12 vid...
