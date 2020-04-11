51a5f9a66ae802b793987ad41eaee3e1115c10cc
Ramanpreet <ramanpreet.singh@vtnetzwelt.com>
2020-04-11 22:24:55 +0530

    follow up commit

--- NextJS-Practise/Docs-Tutorial-Project/.next/server/static/development/pages/index.js
+++ NextJS-Practise/Docs-Tutorial-Project/.next/server/static/development/pages/index.js
@@ -88,7 +88,7 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -1960,7 +1960,7 @@ Index.getInitialProps = async function () {
 
 /***/ }),
 
-/***/ 3:
+/***/ 6:
 /*!******************************!*\
   !*** multi ./pages/index.js ***!
   \******************************/
--- NextJS-Practise/Docs-Tutorial-Project/.next/static/runtime/webpack.js
+++ NextJS-Practise/Docs-Tutorial-Project/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "89aea82128f2953226e1";
+/******/ 	var hotCurrentHash = "adb962616cac63fae922";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
